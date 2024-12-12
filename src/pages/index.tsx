e3990ad17219f747933963016384fb2dca10a9a7
webstudio <sitproweb@gmail.com>
2024-12-12 10:17:20 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,1 +1,28 @@
+import React from 'react';
+import FullPageLayout from '../components/FullPageLayout';
 
+interface HomeProps {
+  data: {
+    sections: any[];
+  };
+}
+
+const Home: React.FC<HomeProps> = ({ data }) => {
+  const navigationTooltips = ['Accueil', 'Services', 'À propos', 'Contact'];
+  const sectionsColor = ['#f2f2f2', '#fff', '#f2f2f2', '#fff'];
+
+  return (
+    <FullPageLayout
+      navigationTooltips={navigationTooltips}
+      sectionsColor={sectionsColor}
+    >
+      {data.sections.map((section, index) => (
+        <section key={index}>
+          {/* Contenu de votre section */}
+        </section>
+      ))}
+    </FullPageLayout>
+  );
+};
+
+export default Home; 
